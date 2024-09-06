fb7a2811494a17cad9d566a058a18c5710b78f29
Tsafack Joël Bertrand <6200224@crosemont.qc.ca>
2024-09-06 11:55:16 -0400

    added buble animation

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
-// this file contain all the basci fonctionalities
+// this file contain all the basic fonctionalities
 
-manageItemText();
-
-
-
-window.addEventListener("resize", ()=>{
-	manageItemText();
-})
 
 function manageItemText(){
 	if(window.screen.availWidth <= 768){
@@ -20,3 +13,34 @@ function manageItemText(){
 	}
 }
 
+manageItemText();
+
+
+// resizing projects item section
+window.addEventListener("resize", ()=>{
+	manageItemText();
+})
+
+
+// bulles
+function createBubles(){
+
+	setInterval(()=>{
+		let div;
+		div = document.createElement('div');
+		div.setAttribute('class', 'buble-item');
+		div.style.top = Math.random()* parseInt(document.getElementById("header").offsetHeight) -100 + "px"; // looks like its a wate of enegy but it handle the resizing by getting the header's offset width
+		div.style.left = Math.random()* parseInt(document.getElementById("header").offsetWidth) -100 + "px";
+		console.log(parseInt(document.getElementById("header").offsetWidth));
+		console.log(div.style.left);
+
+		document.getElementById("header").appendChild(div);
+
+		setTimeout(()=>{
+			document.getElementById("header").removeChild(div);
+		}, 1000);
+	}, 500);
+
+}
+
+createBubles();
