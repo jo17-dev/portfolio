2f52bcda8ce97c7e7ececdf42a5797449a597878
jo17-dev <procompter1@gmail.com>
2026-01-02 20:01:10 -0500

    added notice popup and updated used stack design

--- js/app.js
+++ js/app.js
@@ -1,46 +1,50 @@
-// this file contain all the basic fonctionalities
 
 
-function manageItemText(){
-	if(window.screen.availWidth <= 768){
-		document.querySelectorAll(" .item-description").forEach(item =>{
-			item.style.display = 'block';
-		});
-	}else{
-		document.querySelectorAll(" .item-description").forEach(item =>{
-			item.style.display = 'none';
-		});
-	}
+document.addEventListener("DOMContentLoaded", (e)=>{
+    const windowCover = document.createElement('div');
+    windowCover.setAttribute("id", "window-cover");
+
+    const noticeDescription = "Hey there! This form isn’t quite finished yet. I’m actively working on it. You can still browse and discover who i am, and thanks for your patience!";
+    const noticeTitle ="Notice";
+
+    const workInProgressNoticeStringHTMLElement = createPopupElement(
+        noticeTitle, 
+        noticeDescription,
+        ()=>{windowCover.remove()}
+    );
+
+    windowCover.appendChild(workInProgressNoticeStringHTMLElement);
+
+    document.body.appendChild(windowCover);
+});
+
+
+
+function createPopupElement(title, description, onClickedYes = ()=>{}){
+
+    const popup = document.createElement("div");
+    popup.id = "notice-popup";
+
+    const h3 = document.createElement("h3");
+    h3.className = "notice-title";
+    h3.textContent = title;
+
+    const p = document.createElement("p");
+    p.className = "notice-description";
+    p.textContent = description;
+
+    const footer = document.createElement("footer");
+
+    const button = document.createElement("button");
+    button.className = "btn notice-ok-btn";
+    button.textContent = "I understand";
+
+    button.addEventListener("click", () => {
+        onClickedYes();
+    });
+
+    footer.appendChild(button);
+    popup.append(h3, p, footer);
+
+    return popup;
 }
-
-manageItemText();
-
-
-// resizing projects item section
-window.addEventListener("resize", ()=>{
-	manageItemText();
-})
-
-
-// bulles
-function createBubles(){
-
-	setInterval(()=>{
-		let div;
-		div = document.createElement('div');
-		div.setAttribute('class', 'buble-item');
-		div.style.top = Math.random()* parseInt(document.getElementById("header").offsetHeight) -100 + "px"; // looks like its a wate of enegy but it handle the resizing by getting the header's offset width
-		div.style.left = Math.random()* parseInt(document.getElementById("header").offsetWidth) -100 + "px";
-		console.log(parseInt(document.getElementById("header").offsetWidth));
-		console.log(div.style.left);
-
-		document.getElementById("header").appendChild(div);
-
-		setTimeout(()=>{
-			document.getElementById("header").removeChild(div);
-		}, 1000);
-	}, 500);
-
-}
-
-createBubles();
